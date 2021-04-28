e4ebbd7ecca9b7192276f156569f3f3e7e88261e
rick-25 <araj7060@gmail.com>
2021-04-28 22:53:38 +0530

    Pending : spiral array

--- C_Workspace/leetcode.cpp
+++ C_Workspace/leetcode.cpp
@@ -1,45 +1,38 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+
 using namespace std;
 
-#define ln '\n';
-typedef long long ll;
-
- class Solution {
-public:
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        return solve(obstacleGrid, 0, 0);
-    }
-    
-    int solve(vector<vector<int>>& obstacleGrid, int x, int y) {
-        if(!isValid(obstacleGrid, x, y))    return 0;
-        
-        if(x == obstacleGrid.size()-1 && y == obstacleGrid[0].size()-1)
-            return 1;
-
-        return solve(obstacleGrid, x, y+1) + solve(obstacleGrid, x+1, y); 
-    }
-    
-    bool isValid(vector<vector<int>>& obstacleGrid, int x, int y) {
-        if(x >= obstacleGrid.size() || x < 0 || y >= obstacleGrid[0].size() || y < 0)
-            return false;
-        return (obstacleGrid[x][y] == 0);
-    }
-};
+#define ln '\n'
+#define ll long long
+ 
 
 
 int main() { 
-    //TODO : test here
-
-int main() {    
 #ifndef ONLINE_JUDGE
     freopen("inputf.in", "r", stdin);
     freopen("outputf.in", "w", stdout);
 #endif
-   	//TODO : test here
-    string temp = to_string(3) + "ahelo";
-    cout << temp << ln;
+    
+   	ll n, x;
+   	cin >> n >> x;
+   	
+   	ll hoursSum = 0;
+   	while(n--) 
+   	{
+   	    ll temp;
+   	    cin >> temp;
+   	    hoursSum += temp;
+   	}
+
+   	if(hoursSum <= 24 - x)
+   		cout << "YES" << ln;
+   	else 
+   		cout << "NO" << ln;
+    
     return 0;
 }
 
