e757236bc4ed071b81e9e302629e04ac2081fb83
rick-25 <araj7060@gmail.com>
2021-05-03 10:47:00 +0530

    Doing Infix->Postfixls

--- C_Workspace/main.cpp
+++ C_Workspace/main.cpp
@@ -12,48 +12,75 @@ using namespace std;
 #define ln '\n'
 #define ll long long
 
+// 2
+// 15
+// ((A+B)*D)^(E-F)
+// 19
+// A+(B*C-(D/E^F)*G)*H
 
+// Example Output
 
-void solve() 
+// AB+D*EF-^
+// ABC*DEF^/G*-H*+
+
+void solve()
 {
-	ll n; cin >> n;
-	vector<ll> arr(n);	
-	for(ll i=0; i<n; i++) 
-		cin >> arr[i];
+	ll n;
+	string s;
+	cin >> n >> s;
 
-	stack<pair<ll, ll>> stk;
-	stk.push({arr[0], 0});	
-	
-	for(ll i=1; i<n; i++) 
+	string ans;
+	stack<pair<char, int>> stk;
+	unordered_set<char> charSet = {'+', '-', '*', '/', '^'};
+
+	ll level = 0;
+	for (int i = 0; i < s.size(); i++)
 	{
-		if(arr[i] < stk.top().first) 
-			stk.push({arr[i], i});
+		if (charSet.find(s[i]) != charSet.end())
+		{
+			stk.push({s[i], level});
+		}
+		else if (s[i] == '(')
+		{
+			level++;
+		}
+		else if (s[i] == ')')
+		{
+			while (!stk.empty() && stk.top().second == level)
+			{
+				ans.push_back(stk.top().first);
+				stk.pop();
+			}
+
+			level--;
+		}
+		else
+		{
+			ans.push_back(s[i]);
+		}
 	}
 
-	ll ans = 0, lastIndex = n;
-	while(!stk.empty()) 
+	while (stk.size() > 0)
 	{
-		ans += stk.top().first * (lastIndex - stk.top().second);
-		lastIndex = stk.top().second; 
+		ans.push_back(stk.top().first);
 		stk.pop();
 	}
-	
+
 	cout << ans << ln;
 }
 
-
-int main()  	
+int main()
 {
 
-#ifndef ONLINE_JUDGE	
+#ifndef ONLINE_JUDGE
 	freopen("inputf.in", "r", stdin);
 	freopen("outputf.in", "w", stdout);
 #endif
-    
-    ll t = 1;
-    cin >> t;
-    while(t--)
-	solve();
-	
-    return 0;
+
+	ll t = 1;
+	cin >> t;
+	while (t--)
+		solve();
+
+	return 0;
 }
