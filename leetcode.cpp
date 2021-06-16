74c590f609e5cd6f7d979b7f32529613d344f7a0
Aditya Dwivedy <rick@pop-os.localdomain>
2021-06-16 21:04:07 +0530

    Sudoku solver

--- leetcode.cpp
+++ leetcode.cpp
@@ -5,6 +5,7 @@
 #include <cmath>
 #include <climits>
 #include <algorithm>
+#include <numeric>
 #include <iterator>
 #include <map>
 #include <set>
@@ -19,93 +20,76 @@ using namespace std;
 #define ll long long
 #define MOD 1000000007
 
-class Solution 
-{
+class Solution {
+	int getCubeNo(int r, int c)
+	{
+		int rdif = r / 3, cdif = c / 3;
+		return (rdif*3) + cdif;
+	}
+	bool backtrack(vector<vector<char>>& board, int r, int c, unordered_set<char> rows[], unordered_set<char> cols[], unordered_set<char> cubes[])
+	{
+		if(r == 9)	return true; //Base case
+
+		int tr, tc;
+		if(c == 8) {tr = r+1; tc = 0;}
+		else {tr = r; tc = c+1;}
+
+
+		if(board[r][c] != '.')
+			return backtrack(board, tr, tc, rows, cols, cubes);
+		
+
+		
+		int cubeNo = getCubeNo(r, c);
+		for(char i='1'; i<='9'; i++)
+		{
+			if(rows[r].find(i) == rows[r].end() && cols[c].find(i) == cols[c].end() && cubes[cubeNo].find(i) == cubes[cubeNo].end())
+			{
+				rows[r].insert(i);
+				cols[c].insert(i);
+				cubes[cubeNo].insert(i);
+				board[r][c] = i;
+
+
+				if(backtrack(board, tr, tc, rows, cols, cubes))
+					return true;
+
+				cubes[cubeNo].erase(i);
+				cols[c].erase(i);
+				rows[r].erase(i);
+			}
+		}
+
+		board[r][c] = '.';
+
+		return false;
+	}
 public:
-    bool isPal(std::string str)
-    {
-        int l = 0, r = str.size()-1;
+    void solveSudoku(vector<vector<char>>& board) 
+	{
+		unordered_set<char> rows[9];
+		unordered_set<char> cols[9];
+		unordered_set<char> cubes[9];
 
-        while(l < r)
-            if(str[l++] != str[r--])    return false;
-        
-        return true;
-    }
+		for(int r=0; r<9; r++)
+		{
+			for(int c=0; c<9; c++)
+			{
+				if(board[r][c] == '.')	continue;
 
-    string rev(string str)
-    {
-        string terevmp;
-        for(int i=str.size()-1; i>-1; i--)
-            terevmp.push_back(str[i]);
-        return terevmp;
-    }
-public:
-    vector<vector<int>> palindromePairs(vector<string>& words) 
-    {
-        vector<vector<int>> ans;
-        unordered_map<string, int> revmp;
-        unordered_map<string, int> mp;
-        unordered_set<string> seen;
+				rows[r].insert(board[r][c]);
+				cols[c].insert(board[r][c]);
+				cubes[getCubeNo(r, c)].insert(board[r][c]);
+			}
+		}
 
-        for(int i=0; i<words.size(); i++)
-        {
-            mp[words[i]] = i;
-            revmp[rev(words[i])] = i;
-        }
-
-        for(int i=0; i<words.size(); i++)
-        {
-            if(mp.find("") != mp.end() && mp[""] != i && isPal(words[i]))
-            {
-                vector<int> temp = {i, mp[""]};
-                ans.push_back(temp);
-                temp = {mp[""], i};
-                ans.push_back(temp);
-            }
-            string front, back;
-            for(int j=0, x=words[i].size()-1; j<words[i].size(); j++, x--)
-            {
-                front.push_back(words[i][j]);
-                back.push_back(words[i][x]);
-
-                if(revmp.find(front) != revmp.end() && revmp[front] != i)
-                {
-                    if(j == words[i].size()-1 || isPal(words[i].substr(j+1, words[i].size())))
-                    {
-                        string token = words[i] + ", " + words[revmp[front]];
-                        if(seen.find(token) != seen.end())  continue;
-                        seen.insert(token);
-
-                        vector<int> terevmpAns = {i, revmp[front]};
-                        ans.push_back(terevmpAns);
-                    }
-                }
-                
-                if(mp.find(back) != mp.end() && mp[back] != i)
-                {
-                    if(x == 0 || isPal(words[i].substr(0, x))) 
-                    {
-                        string token = words[mp[back]] + ", " + words[i];
-                        if(seen.find(token) != seen.end())  continue;
-                        seen.insert(token);
-
-                        vector<int> tempAns = {mp[back], i};
-                        ans.push_back(tempAns);
-                    }
-                }
-            }
-        }
-
-        return ans;
+		backtrack(board, 0, 0, rows, cols, cubes);
     }
 };
 
 int main()
 {	//Test the solution code here
-    vector<string> test = {"abcd","dcba","lls","s","sssll"};
-    vector<vector<int>> ans = Solution().palindromePairs(test);
-    for(auto i : ans)
-        cout << i[0] << ", " << i[1] << ln;
+	cout << sizeof(unordered_set<char>);
 	return 0;
 }
 
