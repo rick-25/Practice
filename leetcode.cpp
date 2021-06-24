3172df35f4d9624ceee9c8cee0b6023eaf0000f5
Aditya Dwivedy <rick@pop-os.localdomain>
2021-06-24 20:09:17 +0530

    Out of Boundary Paths

--- leetcode.cpp
+++ leetcode.cpp
@@ -20,53 +20,63 @@ using namespace std;
 #define ll long long
 #define MOD 1000000007
 
+
 class Solution {
-	bool match(vector<vector<int>>& indexmap, string s)
-	{
-		int pointers[26];
-		for(int i=0; i<26; i++)	pointers[i] = 0;
+	vector<vector<vector<int>>> memo;
 
-		int index = -1;
-		for(int i=0; i<s.size(); i++)
-		{
-			char cur = s[i];
-			int ind = cur - 'a';
+	//Posible moves (Up, Right, Down, Left)
+	int rows, coloums;
+	int maxMoves;
+	int vec[4][2] = {
+		{-1, 0},
+		{0, 1},
+		{1, 0},
+		{0, -1}
+	};
 
-
-			for( ; pointers[ind] < indexmap[ind].size() && indexmap[ind][pointers[ind]] <= index; pointers[ind]++);
-			if(pointers[ind] >= indexmap[ind].size())
-				return false;
-
-
-			index = indexmap[ind][pointers[ind]];
-			pointers[ind]++;
-		}
-
+	bool isValid(int r, int c) {
+		if(r < 0 || c < 0 || r >= rows || c >= coloums)
+			return false;
 		return true;
 	}
-public:
-    int numMatchingSubseq(string s, vector<string>& words) {
 
-        vector<vector<int>> indexmap(26);
-        for(int i=0; i<s.size(); i++)
-			indexmap[s[i]-'a'].push_back(i);
+	int dfs(int r, int c, int remMoves) {
+
+		if(!isValid(r, c))	
+			return 1;
+		if(!remMoves)
+			return 0;
+		if(memo[r][c][remMoves] > -1)
+			return memo[r][c][remMoves];
 
 		int ans = 0;
-		for(int i=0; i<words.size(); i++)
-		{
-			if(words[i].size() <= s.size() && match(indexmap, words[i]))
-				ans++;
-		}
+		for(int i=0; i<4; i++) 
+			ans = (ans % MOD + dfs(r+vec[i][0], c+vec[i][1], remMoves-1) % MOD) % MOD;
+		
 
-		return ans;
+		memo[r][c][remMoves] = ans;
+		return memo[r][c][remMoves];
+	}
+
+public:
+	//The idea is to do dfs to count all possible path and also use dp to store ans for each cell(since cycle is allowed o.O)
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+
+		memo = vector<vector<vector<int>>>(m, vector<vector<int>>(n, vector<int>(maxMove+1, -1)));  
+		rows = m;
+		coloums = n;
+		maxMoves = maxMove;
+
+		return dfs(startRow, startColumn, maxMove);
     }
 };
 
 int main()
 {	//Test the solution code here
-	vector<string> test {"ahjpjau","ja","ahbwzgqnuk","tnmlanowax"};
-	string root = "dsahjpjauf";
-	cout << Solution().numMatchingSubseq(root, test) << ln;
+
+	Solution object;
+	cout << object.findPaths(1, 3, 3, 0, 1) << ln;
+
 	return 0;
 }
 
