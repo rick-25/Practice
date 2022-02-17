306ca5aadfa90b4e2f0125d0b1707b20df7b7410
Aditya Kumar <araj7060@gmail.com>
2022-02-17 19:29:37 +0530

    Updated cp template

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include <iostream>
 #include <vector>
 #include <stack>
@@ -21,33 +22,11 @@ using namespace std;
 #define MOD 1000000007
 
 
-class Solution {
-  void help(unordered_map<int, int>& table, vector<int>& arr1, vector<int>& arr2) {
-    for(auto i : arr1) {
-      for(auto j : arr2) {
-        table[i + j]++;
-      }
-    }
-  }
-public:
-    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
-      int ans = 0;
-      unordered_map<int, int> table1;
-      unordered_map<int, int> table2;
-      help(table1, nums1, nums2);
-      help(table2, nums3, nums4);
-      for(auto p : table1) {
-        int target = p.first * -1;
-        ans += p.second * table2[target];
-      }
-      return ans;        
-    }
-};
 
 
 int main(int argLen, const char** args)
-{	  //Test the Solution code here
-  cout << "Hello" << ln;
+{	//Test the Solution code here
+    cout << ln;
     return 0;
 }
 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
+#include <functional>
+#include <iostream>
 #include <algorithm>
 #include <climits>
 #include <cmath>
-#include <iostream>
 #include <iterator>
 #include <map>
 #include <queue>
@@ -21,109 +22,32 @@ using namespace std;
 #define MOD 1000000007
 
 
-class DisjointSet {
-    int* parent;
-    int* size;
-    public:
-    DisjointSet(int len) {
-        parent = new int[len];
-        size = new int[len];
-    }
-    int find_set(int v) {
-        if (v == parent[v])
-            return v;
-        return parent[v] = find_set(parent[v]);
-    }
-    void make_set(int v) {
-        parent[v] = v;
-        size[v] = 1;
-    }
-    void union_sets(int a, int b) {
-        a = find_set(a);
-        b = find_set(b);
-        if (a != b) {
-            if (size[a] < size[b])
-                swap(a, b);
-            parent[b] = a;
-            size[a] += size[b];
-        }
-    }
-};
-
 void solve() {
-    //if we can track path from unsorted-el position to sorted-el postion 
-    //for every el then we can say that input can be sorted 
-    //as per given condition
     ll n; cin >> n;
     vector<ll> v(n);
-    vector<ll> sorted(n);
-
-    DisjointSet ds(33);
-
-    for(int i=0; i<n; i++) {
-        cin >> v[i];
-        sorted[i] = v[i];
-        int last = -1;
-        for(int j=0; j<32; j++) {
-            if(v[i] & (1 << j)) {
-                if(last != -1) 
-                    ds.union_sets(j, last);
-                else 
-                    ds.make_set(j);
-                last = j;
-            }
-        }
-    }
-    sort(sorted.begin(), sorted.end());
-
-
-    bool swapable = false;
-
-    for(int i=0; i<n; i++) {
-
-        const int a = v[i];
-        const int b = sorted[i];
-        //If we can't swap a and b then we can't sort the array
-        
-        swapable = false;
-
-        for(int i=0; i<32 && !swapable; i++) { //for a 
-            if(!(a & (1 << i)))
-                continue;
-            for(int j=0; j<32; j++) { //for b
-                if(!(b & (1 << j)))
-                    continue;
-                if(ds.find_set(i) == ds.find_set(j)) {
-                    swapable = true;
-                    break;
-                }
-            }
-        }
-
-        if(!swapable)
-            break;
-    }
-
-
-    if(swapable)
-        cout << "YES\n";
-    else
-        cout << "NO\n";
+    for(auto& i: v)
+        cin >> i;
+    
 }
 
 
 
 int main() {
+    //Fast IO
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
 #ifndef ONLINE_JUDGE
     cout << ln;
     freopen("inputf.in","r",stdin);
-    //freopen("output.txt","w",stdout);
 #endif
     ll t = 1;
 
     cin >> t;
-    while (t--)
+    while (t--) {
         solve();
+        cout << ln;
+    }
 
     return 0;
 }
